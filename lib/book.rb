c622f1ee6ce8dd33e650ba47ffc35a4e171a583f
katampoi <simonkatampoi@student.moringaschool.com>
2022-08-21 12:23:37 +0300

    Complete Assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
-
+class Book
+    attr_accessor :title, :author, :genre, :page_count
+  
+    def initialize(title)
+        @title = title
+    end
+  
+    def turn_page
+       puts " Flipping the page...wow, you read fast!"
+    end
+  end
+  
+  test = Book.new('manufacturing')
+  puts test.turn_page
+  puts test.title
 
